b4af70ec298913f80386a517dabb06a7998d6dae
Marco <marcosimoes1998@gmail.com>
2021-01-12 15:29:16 -0300

    basic components

--- project_questions/lib/main.dart
+++ project_questions/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 main () {
+  // runApp receives a root widget that is going to contain all other widgets that make the aplication
   runApp(new AppQuestion());
 }
 
@@ -8,7 +9,13 @@ class AppQuestion extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Text('Hello Flutter'),
+      //scaffold is used to create your app structure
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Hello Flutter'),
+      ),
     );
   }
 }
